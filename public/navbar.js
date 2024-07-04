7f3c6e2039ae50d5021a86889a73f23923e1907a
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-04 16:23:06 +0530

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -40,6 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 console.log("Logout successful");
                                 // Redirect to index page and prevent going back
                                 location.replace('index.html');
+                                document.cookie = 'jwtoken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=None; Secure';
                                 setTimeout(checkAuthAgain, 1000); // Ensure check-auth runs after some time
                             } else {
                                 console.error('Logout failed');
