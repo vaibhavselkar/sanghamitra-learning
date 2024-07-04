5998dfe87ab8cf4e1d62046ba422169e867b121b
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-04 16:55:04 +0530

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             });
                             if (response.ok) {
                                 console.log("Logout successful");
-                                document.cookie = 'jwtoken=; Domain=sanghamitra-learning.vercel.app; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; Secure';
+                                document.cookie = 'jwtoken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; Secure';
                                 location.replace('index.html');
                             } else {
                                 console.error('Logout failed');
@@ -62,40 +62,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         .catch(error => console.error('Error checking authentication:', error));
-    }
-
-    // Function to check authentication again after attempting to log out
-    async function checkAuthAgain() {
-        try {
-            const response = await fetch('https://sanghamitra-learning-backend.vercel.app/check-auth', {
-                method: 'GET',
-                credentials: 'include'
-            });
-            const data = await response.json();
-            if (data.authenticated) {
-                // User is still logged in, try to log out again
-                console.log("Retrying logout...");
-                try {
-                    const response = await fetch('https://sanghamitra-learning-backend.vercel.app/logout', {
-                        method: 'GET',
-                        credentials: 'include'
-                    });
-                    if (response.ok) {
-                        console.log("Logout successful on retry");
-                        // Redirect to index page and prevent going back
-                        location.replace('index.html');
-                    } else {
-                        console.error('Logout failed on retry');
-                    }
-                } catch (error) {
-                    console.error('Error during logout on retry:', error);
-                }
-            } else {
-                console.log("User successfully logged out");
-            }
-        } catch (error) {
-            console.error('Error checking authentication again:', error);
-        }
     }
 
     loadNavbar();
